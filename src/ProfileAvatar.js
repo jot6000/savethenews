8b08304c1395c07b82790e5df9f403c9c31ae411
Colette <colette.rs.shaw@gmail.com>
2019-09-15 11:51:08 +0100

    add different avatar layouts

--- src/ProfileAvatar.js
+++ src/ProfileAvatar.js
@@ -5,12 +5,21 @@ import placeholderAvatar from "./placeholderAvatar.jpg";
 import basicStyling from './stylesheet.css';
 
 function Avatar(props) {
-  return ( <div>
-      <img id="avatar" class="profile" src={props.avatar}/>
-      <p id="username" class="profile">{props.username}</p>
-    </div> );
+  const imageOverName = props.imageOverName;
+  const imageNextToName = props.imageNextToName;
+  if (imageOverName) {
+    return ( <div>
+        <img id="avatar" class="profile" src={props.avatar}/>
+        <p id="username" class="profile">{props.username}</p>
+      </div> );
+    } else {
+      // const profileDetails = document.getElementById("profileDetails");
+      // profileDetails.classList.add("horizontal");
+      return ( <div id="horizontalProfile">
+          <img id="avatar" class="profile" src={props.avatar}/>
+          <p id="username" class="profile">{props.username}</p>
+        </div> );
+    }
 }
-
-
 
 export default Avatar;
